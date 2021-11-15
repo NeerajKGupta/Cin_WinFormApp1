0be9fd52d44fb237db81ae6f1f6436fa46188b09
NeerajGupta <nk.gupt@gmail.com>
2021-11-15 13:38:24 +0530

    cm1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,7 @@ namespace WindowsFormsApp1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(336, 308);
+            this.button1.Location = new System.Drawing.Point(302, 394);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
